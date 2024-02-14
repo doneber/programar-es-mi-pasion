ffa8868383df054fd07f41073dc345381ef63e9c
Eber <doneber98@gmail.com>
2024-02-14 14:43:55 -0400

    feat: agrega la opcion de leer mensajes personalizados

--- carta-san-valentin/script.js
+++ carta-san-valentin/script.js
@@ -1,3 +1,18 @@
+// read custom message from query strings
+// Tutorial -> https://youtu.be/6ojp1iWUKw8
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+
+const messageCustom = urlSearchParams.get('message')
+
+if (messageCustom) {
+
+  const mainMessageElement = document.querySelector('#mainMessage')
+  mainMessageElement.textContent = decodeURI(messageCustom)
+}
+
+// the tutorial starts here
+
 const btnOpenElement = document.querySelector('#open')
 const btnCloseElement = document.querySelector('#close')
 
